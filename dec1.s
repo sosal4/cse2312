b2191fd88d642316b49bc6f8dcd347761e952613
sosal4 <sosal4@hotmail.com>
2016-12-10 04:25:34 -0600

    Update dec1.s

--- dec1.s
+++ dec1.s
@@ -17,6 +17,7 @@ main:
 	VMOV S0, R0             @ move return value R0 to FPU register S0
     	VCVT.F64.F32 D1, S0     @ covert the result to double precision for printing
     	VMOV R1, R2, D1         @ split the double VFP register into two ARM registers
+	BL printabs
 	
 
 	BL getchar
