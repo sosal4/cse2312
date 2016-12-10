de98319d35a2b265d945af1b8c93bc441f3aed67
sosal4 <sosal4@hotmail.com>
2016-12-10 00:39:10 -0600

    Update dec1.s

--- dec1.s
+++ dec1.s
@@ -1,37 +1,99 @@
-.global main
-.func main
-   
-main:
-    MOV R0, #22             @ load the numerator
-    MOV R1, #7              @ load the denominator
-    VMOV S0, R0             @ move the numerator to floating point register
-    VMOV S1, R1             @ move the denominator to floating point register
-    VCVT.F32.U32 S0, S0     @ convert unsigned bit representation to single float
-    VCVT.F32.U32 S1, S1     @ convert unsigned bit representation to single float
-	
-    VDIV.F32 S2, S0, S1     @ compute S2 = S0 * S1
-    
-    VCVT.F64.F32 D4, S2     @ covert the result to double precision for printing
-    VMOV R1, R2, D4         @ split the double VFP register into two ARM registers
-    BL  _printf_result      @ print the result
-    
-    B   _exit               @ branch to exit procedure with no return
-   
-_exit:  
-    MOV R7, #4              @ write syscall, 4
-    MOV R0, #1              @ output stream to monitor, 1
-    MOV R2, #21             @ print string length
-    LDR R1, =exit_str       @ string at label exit_str:
-    SWI 0                   @ execute syscall
-    MOV R7, #1              @ terminate syscall, 1
-    SWI 0                   @ execute syscall
+/*************************************************************************
+* @file calculator.s
+* @brief simple get keyboard character and compare
+*
+* Simple program of invoking syscall to retrieve a char and two integerd * numbers from keyboard, 
+* and perform the desired operation 
+*
+* @author Sosal Pokhrel   ID:1001202713
+*************************************************************************/
 
-_printf_result:
-    PUSH {LR}               @ push LR to stack
-    LDR R0, =result_str     @ R0 contains formatted string address
-    BL printf               @ call printf
-    POP {PC}                @ pop LR from stack and return
+	.global main
+	.func main
+
+main:
+	BL _scanf				@ branch to scanf prodecure with return
+	MOV R9, R0				
+	BL getchar
+	MOV R10, R0
+	@BL _scanf
+	@MOV R8, R0
+	MOV R1, R9
+	MOV R2,	R10
+	@MOV R3, R8
+	BL comparing
+	MOV R12, R0
+	BL printing
+	B main
+
+getchar:
+	MOV R7, #3 				@ writing syscall, 4
+	MOV R0, #0				@ output the stream to the monitor, 1
+	MOV R2, #1				@ reading a single character
+	LDR R1, =read_char		@ storing the character in data memory
+	SWI 0					@ executing the system call
+	LDR R0, [R1]			@ moveing the character to the return register
+	AND R0, #0xFF			@ mask out all but the lowest 8 bits
+	MOV PC, LR 				@ return
+
+
+
+
+comparing:
+	MOV R4, LR
+	CMP R2, #'a'			@ comparing against the constant char '+'
+	BLEQ abs				@ branch to make equal handler
+	CMP R2, #'-'			@ comparing against the constatn char '-'
+	BLEQ subtracting				@ branch to equal handler
+	CMP R2, #'*'			@ compare against the constatn char '*'
+	BLEQ multiplying				@ branch to equal handler
+	CMP R2, #'M'			@ compare against the constatn char 'M'
+	BLEQ maximize				@ branch to equal handler
+	MOV PC, R4
+	
+	
+_scanf:
+	MOV R4, LR 				@ store LR since scanf call overwrites
+	SUB SP, SP, #4			@ make romm on stack
+	LDR R0, =format_str		@ R0 contains address of format string
+	MOV R1, SP 				@ move SP to R1 to store entry on stack
+	BL scanf 				@ call scanf
+	LDR R0, [SP]  			@ load value at SP into R0
+	ADD SP, SP, #4			@ restore the stack pointer
+	MOV PC, R4				@ return
+
+
+
+printing:
+	MOV R4, LR 				@ store LR since printf call overwrites
+	LDR R0, =print_str		@ R0 contains formatted string address
+	MOV R1, R12				@ R8 contains printf argument (redundant line)
+	BL printf 				@ call printf
+	MOV PC, R4				@ return
+
+abs: 
+	sra r8,r4,31   
+	xor r4,r4,r8   
+	sub r4,r4,r8   
+	@ r4 now has the absolute value now need to print 
+	MOV PC, LR
+
+subtracting:
+	SUB R0, R1, R3
+	MOV PC, LR
+
+multiplying:
+	MUL R0, R1, R3
+	MOV PC, LR
+
+maximize:
+	CMP R1, R3
+	MOVGT R0, R1
+	MOVLT R0, R3
+	MOV PC, LR
+
 
 .data
-result_str:     .asciz      "Pi is approximately 22/7 = %f \n"
-exit_str:       .ascii      "Terminating program.\n"
+format_str:		.asciz		"%d"
+read_char:		.ascii		" "
+print_str:		.asciz		"%d\n"
