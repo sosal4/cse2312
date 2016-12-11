8829c72c0c2dd0551a949b801da4525ce3391675
sosal4 <sosal4@hotmail.com>
2016-12-11 07:01:39 -0600

    Update dec1.s

--- dec1.s
+++ dec1.s
@@ -87,8 +87,8 @@ prepower:
 	MOV R8, R0
 	SUB R8, R8, #1
 	MOV R4, #0
-	MOV R6, #1              @ load the denominator
-    	VMOV S1, R6             @ move the denominator to floating point register
+	MOV R0, #1              @ load the denominator
+    	VMOV S1, R0             @ move the denominator to floating point register
         VCVT.F32.U32 S1, S1     @ convert unsigned bit representation to single float    	
 	
 power:
